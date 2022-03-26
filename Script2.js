58de1e18df07a82946a4ecb653c1edbd5b14e1f3
poojasahu157 <93066172+poojasahu157@users.noreply.github.com>
2022-03-26 23:08:54 +0530

    Update Script2.js

--- Script2.js
+++ Script2.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded" ,() => {
     const allCharacters ={
         Uppercase : "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
         Lowercase: "abcdefghijklmnopqrstuvwxyz",
-        Numbercase: "0123456789",
+        Numbers: "0123456789",
         Symbols: "!@#$%^&*()_+?><|/}~"
     }
     let length = parseInt(range.value);
